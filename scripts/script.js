1c69ffba2e3afa187dfd34c5d2297b1cc292adec
sachinsingh24 <77788607+sachinsingh24@users.noreply.github.com>
2022-01-21 16:43:14 +0530

    error resolved

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+/** @format */
+
 'use strict';
 // Initialise Variables
 const currency = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
@@ -8,35 +10,34 @@ const cash = document.getElementById('cash');
 const balance = document.getElementById('balance');
 const num = document.querySelectorAll('#count');
 const currency_html = document.querySelectorAll('#currency');
-let change;
 
 // functions
 //calculate currency
-const calculate = () => {
-  if (bill_Amt.value <= cash.value) {
+const calculate = (event) => {
+  event.preventDefault();
+  let change = 0;
+  if (parseInt(bill_Amt.value) <= parseInt(cash.value)) {
     change = cash.value - bill_Amt.value;
     balance.innerText = change;
+    // console.log(change);
   } else {
     change = bill_Amt.value - cash.value;
-    if (change >= 0) {
-      balance.innerText = ` -${change} Cash is less then Bill Amount`;
-    } else {
-      balance.innerText = `Try Again`;
-    }
+    // console.log(change);
+    balance.innerText = `-${change} Cash is less then Bill Amount`;
   }
-  checking();
+  checking(change);
 };
 
 // checking countable currency
-const checking = () => {
+const checking = (value) => {
   for (let i = 0; i < 10; i++) {
     count.push(0);
-    console.log(count);
-    while (change >= currency[i]) {
-      change = change - currency[i];
+    // console.log(count);
+    while (value >= currency[i]) {
+      value = value - currency[i];
       count[i]++;
     }
-    console.log(count[i]);
+    // console.log(count[i]);
     num[i].innerText = count[i];
     if (count[i] > 0) {
       num[i].classList.add('hightlight1');
